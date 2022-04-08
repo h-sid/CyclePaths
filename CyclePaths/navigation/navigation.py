4a389ae4dd4e116492a00f290a905a899b21582d
h-sid <55513127+h-sid@users.noreply.github.com>
2022-04-08 11:25:58 +0100

    Update navigation.py

--- CyclePaths/navigation/navigation.py
+++ CyclePaths/navigation/navigation.py
@@ -14,7 +14,7 @@ os.system('gsutil cp gs://os-road-data-hackathon/data_with_estimate.pq .')
 gdf = gpd.read_parquet('data_with_estimate.pq')
 
 
-@lru_cache(max_size=none)
+@lru_cache(max_size=None)
 def googleMapsSucks(startRoad, endRoad, dangerLevel):
 
     def cyclingWeight(row):
